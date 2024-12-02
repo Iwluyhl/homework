f6c332bb9efdbe5556b9baad7f9f157daff1a9a6
Iwluyhl <mih.molodtsov@mail.ru>
2024-12-02 18:16:49 +0300

    module_3_2

--- module_3_2.py
+++ module_3_2.py
@@ -14,9 +14,9 @@ def send_email(messege, recipient, sender = "university.help@gmail.com"):
         
 
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email('проверка связи', 'vasyok1337@gmail.com')
+send_email('привет', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('hellp', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('hi', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
 
 
