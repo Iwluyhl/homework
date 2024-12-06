25f2cafd45658a071300b05a20ac4522e094f43f
Iwluyhl <mih.molodtsov@mail.ru>
2024-12-06 10:09:33 +0300

    module_3_1

--- module_3_1.py
+++ module_3_1.py
@@ -1,20 +1,24 @@
 calls = 0
 
+
 def count_calls():
     global calls
     calls += 1
-    
-def string_info (string):
+
+
+def string_info(string):
     count_calls()
     return len(string), string.upper(), string.lower()
 
-def is_contains (string, list_to_search):
+
+def is_contains(string, list_to_search):
     count_calls()
     search_string = string.lower()
     for item in list_to_search:
         if search_string == item.lower():
             return True
     return False
+
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
