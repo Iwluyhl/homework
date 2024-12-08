0e5e8d75193ef8f34f602ccf852bc79bfa3cb5ba
Iwluyhl <mih.molodtsov@mail.ru>
2024-12-08 13:10:41 +0300

    module_3_5 испр

--- module_3_5.py
+++ module_3_5.py
@@ -1,11 +1,14 @@
-def get_multiplied_digits (number):
+def get_multiplied_digits(number):
     str_number = str(abs(number))
     first = int(str_number[0])
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
-    else:
+    if str_number[-1] == '0':
+        str_number = str_number[: -1]
+    if len(str_number) <= 1:
         return first
-    
-number = 40203
-result = get_multiplied_digits(number)
-print(f"Произведение цифр числа {number}: {result}")
+    if len(str_number) >= 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+
+result = get_multiplied_digits(40203) # ожидаемый ответ 24
+print(result)
+result2 = get_multiplied_digits(402030) # ожидаемый ответ 24
+print(result2)
